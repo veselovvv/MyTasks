7f7f9c05b4fb8bf4a0f56c1219c1e6f9384143a9
Viacheslav Veselov <vycheslav5veselov@gmail.com>
2021-12-26 19:08:59 +0200

    Fixed imports

--- app/src/main/java/com/veselovvv/mytasks/MainViewModel.java
+++ app/src/main/java/com/veselovvv/mytasks/MainViewModel.java
@@ -2,9 +2,7 @@ package com.veselovvv.mytasks;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
-
 import java.util.List;
-
 import com.veselovvv.mytasks.Task;
 import com.veselovvv.mytasks.App;
 
