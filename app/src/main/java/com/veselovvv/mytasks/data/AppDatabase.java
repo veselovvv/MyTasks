1886a788efcf75dddcc32742be2b1afad25a6625
Viacheslav Veselov <vycheslav5veselov@gmail.com>
2021-12-26 19:00:29 +0200

    Fixed imports

--- app/src/main/java/com/veselovvv/mytasks/data/AppDatabase.java
+++ app/src/main/java/com/veselovvv/mytasks/data/AppDatabase.java
@@ -2,7 +2,6 @@ package com.veselovvv.mytasks.data;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
-
 import com.veselovvv.mytasks.Task;
 
 @Database(entities = {Task.class}, version = 1, exportSchema = false)
